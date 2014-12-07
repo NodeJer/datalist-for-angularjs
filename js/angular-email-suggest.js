53f52c5c37c57cb412ffffab6fda05231d04b11b
1245089789@qq.com <taoye1245089789@gmail.com>
2014-12-07 20:22:56 +0800

    first

--- js/angular-email-suggest.js
+++ js/angular-email-suggest.js
@@ -1,3 +1,14 @@
+
+var ie = (function(){
+    var v = 3, div = document.createElement('div'), all = div.getElementsByTagName('i');
+
+    while (div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->', all[0]);
+    
+    return v > 4 ? v : false;
+}());
+
+alert(ie);
+
 angular.module('datalist', []).
 
 run(function($rootScope){
